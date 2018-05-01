a631d015720ec3f009ee763911eee7c8d4360077
LazyPlazy <joshua@marketingdepot.ca>
2018-05-01 17:55:35 -0600

    fixed oauth callback

--- config/oauth-setup.js
+++ config/oauth-setup.js
@@ -6,13 +6,18 @@ var Users = require("../models/user");
 passport.use(new FacebookStrategy({
     clientID: keys.facebook.clientID,
     clientSecret: keys.facebook.clientSecret,
-    callbackURL: "https://marketingdepot.herokuapp.com/oauth/facebook/redirect"
+    callbackURL: "https://marketingdepot.herokuapp.com/oauth/facebook/redirect",
+    profileFields: ['id', 'displayName', 'first_name', 'last_name', 'email'],
+    enableProof: true
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOne({ facebookId: profile.id }, function (err, user) {
       if(currentUser){
         //  already has an account
+        passport.authenticate("facebook")(req, res, function(){
+           res.redirect("/");
       } else {
+        //  user doesnt have an account
         var newUser = new User({
           username: profile.email,
           email:  profile.email,
