421894cd5ae3d3041ceaa65946d944222d7248ee
LazyPlazy <joshua@marketingdepot.ca>
2018-05-01 16:38:04 -0600

    changed oauth callback uri to be https

--- config/oauth-setup.js
+++ config/oauth-setup.js
@@ -6,7 +6,7 @@ var Users = require("../models/user");
 passport.use(new FacebookStrategy({
     clientID: keys.facebook.clientID,
     clientSecret: keys.facebook.clientSecret,
-    callbackURL: "/auth/facebook/redirect"
+    callbackURL: "https://infinite-stream-28721.herokuapp.com/auth/facebook/redirect"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
