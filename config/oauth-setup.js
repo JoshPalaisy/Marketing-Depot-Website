2f2cbb4e6fb3acde4c6544cec6a765378ae06e75
LazyPlazy <joshua@marketingdepot.ca>
2018-05-01 18:38:26 -0600

    fixed oauth syntax and local to facebook for authenticate

--- config/oauth-setup.js
+++ config/oauth-setup.js
@@ -7,25 +7,17 @@ passport.use(new FacebookStrategy({
     clientID: keys.facebook.clientID,
     clientSecret: keys.facebook.clientSecret,
     callbackURL: "https://marketingdepot.herokuapp.com/oauth/facebook/redirect"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOne({ facebookId: profile.id }, function (err, user) {
-      if(currentUser){
-        //  already has an account
-      } else {
-        new User({
-          username: profile.email,
-          email: profile.email,
-          first_name: profile.first_name,
-          last_name: profile.last_name,
-          facebookId: profile.id
-        }).save().then((newUser, function(req, res){
-          console.log("New user created:" + newUser);
-          passport.authenticate("local")(req, res, function() {
-            res.redirect("/");
-            });
-        }));
-      }
-    });
-  }
-));
+  },function(accessToken, refreshToken, profile, done){
+    new User({
+      username: profile.email,
+      email: profile.email,
+      first_name: profile.first_name,
+      last_name: profile.last_name,
+      facebookId: profile.id
+    }).save().then((newUser, function(req, res){
+      console.log("New user created:" + newUser);
+      passport.authenticate("facebook")(req, res, function() {
+        res.redirect("/");
+        });
+    }));
+  }));
